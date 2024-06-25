6b29e0b7dc5e9a5423a725ad822027f86b03eb2c
Renzo Juli Machaca <151465459+Haname1@users.noreply.github.com>
2024-06-24 15:41:10 -1200

    Add files via upload

---
+++ storage/framework/views/307818ed49d1b3c7315dfbde0f84c2d5.php
@@ -0,0 +1,18 @@
+<div>
+     <?php $__env->slot('header', null, []); ?> 
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <?php echo e(__('categorias')); ?>
+
+        </h2>
+     <?php $__env->endSlot(); ?>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <h1 class="text-2xl text-indigo-500 font-blod text-center">Hola mundo</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore alias earum minus tempora eaque velit, aliquid eos odio eveniet? Esse sunt harum enim nobis ex ipsa soluta dolorum molestiae saepe.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php /**PATH K:\LP2 Proyecto integrador\LARAVEL\alquiler\resources\views/livewire/category-main.blade.php ENDPATH**/ ?>
